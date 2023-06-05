6beb3d6e49dc4954b78573a8deaed043dcb23931
Ritesh <Riteshk229@gmail.com>
2023-06-05 20:14:08 +0530

    learnt about eventlistener in react

--- src/Cartitems.js
+++ src/Cartitems.js
@@ -1,20 +1,54 @@
 import React from "react";
 
 class Cartitems extends React.Component{
+
+    constructor(){
+        super();
+        this.state ={
+            title: "Phone",
+            price: "999",
+            qty: 2,
+            image : " "
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    };
+
+    increaseQuantity = ()=> {
+        console.log("this.state",this.state);
+    }
+
     render(){
+        const {price,title,qty} = this.state;
         return (
             <div className="cart-item">
             <div className="left">
             <img style={styles.image}/>
+            {/* <img alt='Product Image' style={styles.image}/> */}
             </div>
             <div className="right">
             <div className="product-details">
-            <div style={{fontSize : 30}}>Phone</div>
-            <div style={{color:"grey"}}>Rs 999</div>
-            <div style={{color:"grey"}}>Qty: 2</div>
+            <div style={{fontSize : 30}}>{title}</div>
+            <div style={{color:"grey"}}>Rs {price}</div>
+            <div style={{color:"grey"}}>Qty : {qty}</div>
             </div>
             <div className="cart-item-actions">
-            
+                <img 
+                    alt="delete"
+                    className="action-icons" 
+                    src="https://www.svgrepo.com/show/502608/delete-2.svg"
+                 />
+                <img 
+                    alt="increase" 
+                    className="action-icons" 
+                    src="https://www.svgrepo.com/show/507821/plus-circle.svg"
+                    onClick={this.increaseQuantity}
+                />
+                <img 
+                    alt="decrease" 
+                    className="action-icons" 
+                    src="https://www.svgrepo.com/show/506729/minus-circle.svg"
+                    
+                />
             </div>
             </div>
             </div>
@@ -26,7 +60,8 @@ class Cartitems extends React.Component{
         image: {
             height: 125,
             width: 125,
-            borderRadius: 4
+            borderRadius: 4,
+            backgroundColor: "#ccc"
         }
     }
     
